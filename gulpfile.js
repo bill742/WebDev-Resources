ec2de6de73cd5d98bf763dd79795216ae63a0a0c
Bill Dean <billdean@Melissa-Girimontes-MacBook-Pro.local>
2016-02-02 23:32:21 -0500

    Updating gulp tasks to output files to production dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     gulpif = require('gulp-if'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat');
 
 var env,
     outputDir,
     jsSources,
     sassStyle;
 
-env = process.env.NODE_ENV || 'development';
+env = process.env.NODE_ENV || 'production';
 
 if (env==='development') {
   outputDir = 'development/';
@@ -19,7 +20,7 @@ if (env==='development') {
 }
 
 jsSources = [
-  'development/js/app.js', 
+  'development/js/app.js',
   'development/js/controllers.js'
 ];
 
@@ -31,32 +32,64 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(outputDir + 'css'))
     .pipe(browserSync.reload({
       stream: true
-    }))
+    }));
+});
+
+gulp.task('css', function(){
+	gulp.src(['development/css/bootstrap-theme.min.css', 'development/css/bootstrap.min.css'],{base: 'development/css/'})
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir + 'css')))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('js', function() {
   gulp.src(jsSources)
       // .pipe(concat('scripts.js'))
+      .pipe(gulpif(env === 'production', concat('scripts.js')))
       .pipe(gulp.dest(outputDir + 'js'))
       .pipe(browserSync.reload({
         stream: true
-      }))
+      }));
 });
 
 gulp.task('json', function(){
   gulp.src('development/js/*.json')
-    //.pipe(gulpif(env === 'production', jsonminify()))
-    .pipe(gulpif(env === 'production', gulp.dest('production/js')))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'js')))
     .pipe(browserSync.reload({
       stream: true
-    }))
-})
+    }));
+});
+
+gulp.task('html', function(){
+	gulp.src('development/*.html')
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('partials', function(){
+	gulp.src('development/partials/*')
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir + 'partials')))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('misc', function(){
+	gulp.src(['development/lib/*', 'development/fonts/*'], {base: 'development/'})
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
 gulp.task('watch', ['browserSync', 'js', 'json', 'sass'], function(){
-  gulp.watch('development/css/*.scss', ['sass']); 
-  gulp.watch('development/*.html', browserSync.reload); 
-  gulp.watch(jsSources, ['js']); 
-  gulp.watch('development/js/*.json', ['json']); 
+  gulp.watch('development/css/*.scss', ['sass']);
+  gulp.watch('development/*.html', ['html']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('development/js/*.json', ['json']);
 });
 
 gulp.task('browserSync', function() {
@@ -64,7 +97,7 @@ gulp.task('browserSync', function() {
     server: {
       baseDir: 'development'
     },
-  })
-})
+  });
+});
 
-gulp.task('default', ['sass', 'js', 'json', 'watch']);
+gulp.task('default', ['sass', 'css', 'js', 'json', 'html', 'partials', 'misc', 'watch']);
--- production/js/scripts.js
+++ production/js/scripts.js
@@ -31,3 +31,14 @@ myApp.config(['$routeProvider', function($routeProvider) {
 		controller: 'ListController'
 	});
 }]);
+(function(){
+	var linkControllers = angular.module('linkControllers', []);
+
+	// Display submitted links
+	linkControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
+		$http.get('js/data.json').success(function(l) {
+			$scope.links = l;
+		});
+	}]);
+
+})();
